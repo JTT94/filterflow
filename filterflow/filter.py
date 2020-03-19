2780ba73e3fe16fe72459bafd7c9b1a6449485a5
AdrienCorenflos <adrien.corenflos@gmail.com>
2020-03-19 17:10:39 +0000

    Added some architecture for the filter itself.

--- filterflow/filter.py
+++ filterflow/filter.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
-from filterflow.base import State, ObservationBase
+from filterflow.base import State, ObservationBase, InputsBase
+
 
 class ParticleFilter(tf.Module):
 
@@ -13,5 +14,44 @@ class ParticleFilter(tf.Module):
         self._resampling_criterion = resampling_criterion
         self._resampling_method = resampling_method
 
+    def propose(self, state: State, observation: ObservationBase, inputs: InputsBase):
+        """Predict step of the filter
 
+        :param state: State
+            prior state of the filter
+        :param observation: ObservationBase
+            Observation used for look ahead proposal
+        :param inputs: InputsBase
+            Inputs used for prediction
+        :return: Proposed State
+        :rtype: State
+        """
 
+        # Resample the state
+        # Use emitter
+        # Apply transition loglikelihood
+
+    def predict(self, state: State, inputs: InputsBase):
+        """Predict step of the filter
+
+        :param state: State
+            prior state of the filter
+        :param inputs: InputsBase
+            Inputs used for preduction
+        :return: Predicted State
+        :rtype: State
+        """
+        # Use transition model
+
+    def update_weights(self, proposed_state: State, observation: ObservationBase, inputs: InputsBase):
+        """
+        :param proposed_state:
+            proposed state of the filter to be corrected
+        :param observation: ObservationBase
+            observation to compare the state against
+        :param inputs: InputsBase
+            inputs for the observation_model
+        :return: Updated weights
+        """
+        # use emitted proposal
+        # apply log likelihood of observation
