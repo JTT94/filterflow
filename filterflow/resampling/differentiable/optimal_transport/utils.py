Fixed size of state changing as we iterate (happened only in tf.function context as the shapes were placeholders). Added a loss function example. Loglikelihood value looks wrong, needs to be checked

filterflow/resampling/differentiable/optimal_transport/utils.py
@@ -28,7 +28,7 @@ def squared_distances(x: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
     :rtype tf.Tensor
     """
     # x.shape = [B, N, D]
-    xx = tf.reduce_sum(x * x, axis=-1, keepdims=True)
+    xx = tf.reduce_sum(x * x, axis=2, keepdims=True)
     xy = tf.einsum('bnd,bmd->bnm', x, y)
     yy = tf.expand_dims(tf.reduce_sum(y * y, axis=-1), 1)
     return tf.clip_by_value(xx - 2 * xy + yy, 0., float('inf'))

filterflow/transition/random_walk.py
@@ -6,7 +6,8 @@ from filterflow.transition.base import TransitionModelBase
 
 
 class RandomWalkModel(TransitionModelBase):
-    def __init__(self, transition_matrix: tf.Tensor, noise: tfp.distributions.Distribution):
+    def __init__(self, transition_matrix: tf.Tensor, noise: tfp.distributions.Distribution, name='RandomWalkModel'):
+        super(RandomWalkModel, self).__init__(name=name)
         self._transition_matrix = transition_matrix
         self._noise = noise
 
@@ -35,6 +36,6 @@ class RandomWalkModel(TransitionModelBase):
         :rtype: State
         """
         pushed_particles = tf.linalg.matvec(self._transition_matrix, state.particles)
-        batch_size = state.batch_size
-        n_particles = state.n_particles
-        return pushed_particles + self._noise.sample([batch_size, n_particles])
+
+        res = pushed_particles + self._noise.sample([pushed_particles.shape[0], pushed_particles.shape[1]])
+        return res