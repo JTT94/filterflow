Fix admissibility in ricatti, allow for not propagating the gradient in transport matrix.

filterflow/resampling/differentiable/corrected.py
@@ -14,7 +14,7 @@ class CorrectedRegularizedTransform(ResamplerBase, metaclass=abc.ABCMeta):
 
     # TODO: Document this really nicely
     def __init__(self, epsilon, scaling=0.75, max_iter=100, convergence_threshold=1e-3, ricatti_solver=None,
-                 name='RegularisedTransform'):
+                 propagate_correction_gradient=True, name='RegularisedTransform'):
         """Constructor
 
         :param epsilon: float
@@ -26,11 +26,13 @@ class CorrectedRegularizedTransform(ResamplerBase, metaclass=abc.ABCMeta):
         :param convergence_threshold: float
             Fixed point iterates converge when potentials don't move more than this anymore
         :param ricatti_solver: filterflow.resampling.differentiable.ricatti.solver.RicattiSolver
+        :param propagate_correction_gradient: should you propagate the correction factor gradient
         """
         self.convergence_threshold = tf.cast(convergence_threshold, float)
         self.max_iter = tf.cast(max_iter, tf.dtypes.int32)
         self.epsilon = tf.cast(epsilon, float)
         self.scaling = tf.cast(scaling, float)
+        self.propagate_correction_gradient = tf.cast(propagate_correction_gradient, bool)
         if ricatti_solver is None:
             self.ricatti_solver = RicattiSolver(tf.constant(0.25), tf.constant(10.), tf.constant(1e-3))
         else:
@@ -50,6 +52,8 @@ class CorrectedRegularizedTransform(ResamplerBase, metaclass=abc.ABCMeta):
         # TODO: The real batch_size is the sum of flags. We shouldn't do more operations than we need...
         transport_matrix, _ = transport(state.particles, state.log_weights, self.epsilon, self.scaling,
                                         self.convergence_threshold, self.max_iter, state.n_particles)
-
         transport_correction = self.ricatti_solver(transport_matrix, state.weights)
+        if not self.propagate_correction_gradient:
+            transport_correction = tf.stop_gradient(transport_correction)
+
         return apply_transport_matrix(state, transport_matrix + transport_correction, flags)

filterflow/resampling/differentiable/regularized_transport/plan.py
@@ -5,8 +5,8 @@ from filterflow.resampling.differentiable.regularized_transport.utils import cos
 
 
 @tf.function
-def fillna(tensor, value):
-    return tf.where(tf.math.is_nan(tensor), value, tensor)
+def _fillna(tensor):
+    return tf.where(tf.math.is_finite(tensor), tensor, tf.zeros_like(tensor))
 
 
 @tf.function
@@ -26,13 +26,20 @@ def transport_from_potentials(x, f, g, eps, logw, n):
     :rtype: tf.Tensor[B, N, N]
 
     """
-
+    float_n = tf.cast(n, float)
+    log_n = tf.math.log(float_n)
     cost_matrix = cost(x, x)
-    fg = (tf.expand_dims(f, 2) + tf.expand_dims(g, 1))  # fg = f + g.T
-    temp = (fg - cost_matrix) / eps
-    temp = temp - tf.reduce_logsumexp(temp, 1, keepdims=True) + tf.math.log(n)
+    fg = tf.expand_dims(f, 2) + tf.expand_dims(g, 1)  # fg = f + g.T
+    temp = fg - cost_matrix
+    temp = temp / eps
+
+    temp = temp - tf.reduce_logsumexp(temp, 1, keepdims=True) + log_n
     # We "divide the transport matrix by its col-wise sum to make sure that weights normalise to logw.
-    transport_matrix = tf.math.exp(temp + tf.expand_dims(logw, 1))
+    temp = temp + tf.expand_dims(logw, 1)
+
+    transport_matrix = tf.math.exp(temp)
+
+
     return transport_matrix
 
 
@@ -54,6 +61,7 @@ def transport(x, logw, eps, scaling, threshold, max_iter, n):
     :rtype tf.Tensor[B, N, N]
     """
     float_n = tf.cast(n, float)
+
     uniform_log_weight = -tf.math.log(float_n) * tf.ones_like(logw)
 
     alpha, beta, _, _, total_iterations = sinkhorn_potentials(logw, x, uniform_log_weight, x, eps, scaling, threshold,

filterflow/resampling/differentiable/regularized_transport/sinkhorn.py
@@ -5,7 +5,6 @@ from filterflow.resampling.differentiable.regularized_transport.utils import cos
 
 # This is very much adapted from Feydy's geomloss work. Hopefully these should merge into one library...
 
-
 @tf.function
 def _simple_sinkhorn_loop(log_alpha, log_beta, cost_xy, cost_yx, epsilon, threshold, max_iter):
     epsilon = tf.ones([log_alpha.shape[0], 1]) * epsilon
@@ -28,7 +27,7 @@ def _simple_sinkhorn_loop(log_alpha, log_beta, cost_xy, cost_yx, epsilon, thresh
     def stop_condition(i, _u, _v, update_size):
         n_iter_cond = i < max_iter - 1
         stable_cond = update_size > threshold
-        return tf.reduce_all([n_iter_cond, stable_cond])
+        return tf.logical_and(n_iter_cond, tf.reduce_all(stable_cond))
 
     def body(i, u, v, _update_size):
         new_u, new_v, new_update_size = apply_one(u, v)
@@ -64,7 +63,7 @@ def sinkhorn_loop(log_alpha, log_beta, cost_xy, cost_yx, cost_xx, cost_yy, epsil
 
     def stop_condition(i, _a_y, _b_x, _a_x, _b_y, continue_, running_epsilon):
         n_iter_cond = i < max_iter - 1
-        return tf.logical_and(n_iter_cond, continue_)
+        return tf.logical_and(n_iter_cond, tf.reduce_all(continue_))
 
     def apply_one(a_y, b_x, a_x, b_y, continue_, running_epsilon):
         running_epsilon_ = tf.reshape(running_epsilon, [-1, 1])

filterflow/resampling/differentiable/regularized_transport/utils.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 
 
+def _fillna(tensor):
+    mask = tf.math.is_finite(tensor)
+    return tf.where(mask, tensor, tf.zeros_like(tensor))
+
+
 @tf.function
 def diameter(x, y):
     min_x = tf.math.reduce_min(x, [1, 2])
@@ -27,7 +32,9 @@ def softmin(epsilon: tf.Tensor, cost_matrix: tf.Tensor, f: tf.Tensor) -> tf.Tens
     f_ = tf.reshape(f, (b, 1, n))
     temp_val = f_ - cost_matrix / tf.reshape(epsilon, (-1, 1, 1))
     log_sum_exp = tf.reduce_logsumexp(temp_val, axis=2)
-    return -tf.reshape(epsilon, (-1, 1)) * log_sum_exp
+    res = -tf.reshape(epsilon, (-1, 1)) * log_sum_exp
+
+    return res
 
 
 @tf.function

filterflow/resampling/differentiable/ricatti/solver.py
@@ -2,17 +2,42 @@ import abc
 
 import tensorflow as tf
 
+LOGGER = tf.get_logger()
+
+
+def _memoize(f):
+    """ Memoization decorator for a function taking a single argument """
+
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+# @_memoize
+# @tf.function
+# def _get_centering_matrix(n):
+#     float_n = tf.cast(n, float)
+#     ones = tf.ones([n, 1], dtype=float)
+#     centering_matrix = tf.eye(n, dtype=float) - tf.matmul(ones, ones, transpose_b=True) / float_n
+#     return tf.expand_dims(centering_matrix, 0)
+
 
 @tf.function
-def _make_nil(tensor):
-    tensor = tensor - tf.reduce_mean(tensor, 1, keepdims=True)
-    return tensor - tf.reduce_mean(tensor, 2, keepdims=True)
+def _make_nil(tensor, axis):
+    return tensor - tf.reduce_mean(tensor, axis, keepdims=True)
 
 
 @tf.function
 def _make_admissible(tensor):
-    tensor = _make_nil(tensor)
-    return 0.5 * (tensor + tf.transpose(tensor, perm=[0, 2, 1]))
+    symmetric_tensor = 0.5 * (tensor + tf.transpose(tensor, perm=[0, 2, 1]))
+    row_mean = tf.reduce_mean(symmetric_tensor, 1, keepdims=True)
+    col_mean = tf.reduce_mean(symmetric_tensor, 2, keepdims=True)
+    mean = tf.reduce_mean(symmetric_tensor, [1, 2], keepdims=True)
+
+    return symmetric_tensor - row_mean - col_mean + mean
 
 
 class BaseSolver(tf.Module, metaclass=abc.ABCMeta):
@@ -39,39 +64,51 @@ class Euler(BaseSolver):
 
     def _solve(self, ode_fn, t_0, z_0):
         @tf.function
-        def next(t, x):
+        def _next(t, x):
             return self._next(t, x, ode_fn, self.step_size)
 
-        def body(t, z, _diff):
-            t_, z_ = next(t, z)
-            return t_, z_, tf.reduce_max(tf.abs(z_ - z))
+        def body(t, z, prev_diff, _is_decreasing):
+            t_, z_ = _next(t, z)
+            new_diff = tf.reduce_max(tf.abs(z_ - z) / self.step_size)
+            return t_, z_, new_diff, new_diff < prev_diff
 
-        def stop(t, z, diff):
+        def stop(t, z, diff, is_decreasing):
             t_cond = t < self.max_horizon
             diff_cond = diff > self.convergence_threshold
-            return tf.logical_and(t_cond, diff_cond)
+            return tf.reduce_all([t_cond, diff_cond, is_decreasing])
 
-        final_t, final_z, final_diff = tf.while_loop(stop, body, [t_0, z_0, 2. * self.convergence_threshold])
+        final_t, final_z, final_diff, final_decreasing = tf.while_loop(stop,
+                                                                       body,
+                                                                       [t_0,
+                                                                        z_0,
+                                                                        tf.constant(float('inf')),
+                                                                        tf.constant(True)])
+        if not final_decreasing:
+            tf.print("Ricatti solver didn't converge - this iterate was not corrected. "
+                     "Try with a smaller step size or increase precision in Sinkhorn iterates")
+            tf.print("final error: ", final_diff)
+            return z_0
+
         return final_z
 
     solve = tf.function(_solve)
 
 
-def make_ode_fun(A, B):
+def make_ode_fun(A, transport_matrix):
     @tf.function
     def ode_fn(_t, delta):
-        b_delta = tf.matmul(B, delta)
-        delta_delta = tf.matmul(delta, delta, transpose_b=True)
-        delta_prime = b_delta + tf.transpose(b_delta, [0, 2, 1]) + delta_delta - A
-        # _make_admissible is only there to protect against numerical instability:
-        # as per theory at all time delta_prime should be symmetric and row-col summing to 0
-        return -_make_admissible(delta_prime)
+        delta = _make_admissible(delta)
+        # Do not allow small errors to propagate
+        b_delta = tf.matmul(transport_matrix, delta, transpose_a=True)
+        delta_delta = tf.matmul(delta, delta, transpose_a=True)
+        delta_prime = A - b_delta - tf.transpose(b_delta, [0, 2, 1]) - delta_delta
+        return delta_prime
 
     return ode_fn
 
 
 class RicattiSolver(tf.Module):
-    """This is a adaptation of https://arxiv.org/pdf/1608.08179.pdf with backprop handled by the adjoint method"""
+    """This is a adaptation of https://arxiv.org/pdf/1608.08179.pdf with backprop handled by unrolling autodiff"""
 
     def __init__(self, step_size=0.5, horizon=5., threshold=1e-3, name='RicattiSolver'):
         super(RicattiSolver, self).__init__(name=name)
@@ -79,24 +116,22 @@ class RicattiSolver(tf.Module):
         self.horizon = tf.cast(horizon, float)
         self.threshold = tf.cast(threshold, float)
         self.solver = Euler(self.step_size, self.threshold, self.horizon)
+
         self._routine = tf.custom_gradient(
             lambda A, B: self._routine_without_custom_grad(self.solver, A, B))
 
     @staticmethod
-    def _routine_without_custom_grad(solver: BaseSolver, A: tf.Tensor, B: tf.Tensor):
-        ode_fn = make_ode_fun(A, B)
+    def _routine_without_custom_grad(solver: BaseSolver, A: tf.Tensor, transport_matrix: tf.Tensor):
+        ode_fn = make_ode_fun(A, transport_matrix)
         res = solver.solve(ode_fn, 0., tf.zeros_like(A))
 
+        @tf.function
         def grad(d_delta):
-            d_delta_ = _make_admissible(d_delta)
-            return tf.gradients(res, [A, B], d_delta_)
+            d_delta = _make_admissible(d_delta)
+            grads = tf.gradients(res, [A, transport_matrix], d_delta)
+            return grads
 
         return res, grad
-
-    @staticmethod
-    @tf.function
-    def _make_B(transport_matrix):
-        return tf.transpose(transport_matrix, perm=[0, 2, 1])
 
     @staticmethod
     @tf.function
@@ -109,7 +144,6 @@ class RicattiSolver(tf.Module):
     def __call__(self, transport_matrix, w):
         n_particles = w.shape[1]
         float_n_particles = tf.cast(n_particles, float)
-        B = self._make_B(transport_matrix)
         A = self._make_A(transport_matrix, w, float_n_particles)
-        final_delta = self._routine(A, B)
+        final_delta = self._routine(A, transport_matrix)
         return final_delta

filterflow/smc.py
@@ -9,6 +9,8 @@ from filterflow.resampling.criterion import ResamplingCriterionBase
 from filterflow.transition.base import TransitionModelBase
 from filterflow.utils import normalize
 
+MIN_LOG_WEIGHT = tf.constant(-3.)
+
 
 class SMC(Module):
     def __init__(self, observation_model: ObservationModelBase, transition_model: TransitionModelBase,
@@ -64,16 +66,17 @@ class SMC(Module):
             inputs for the observation_model
         :return: Updated weights
         """
-
+        float_n_particles = tf.cast(state.n_particles, float)
         proposed_state = self._proposal_model.propose(state, inputs, observation)
         log_weights = self._transition_model.loglikelihood(state, proposed_state, inputs)
         log_weights = log_weights + self._observation_model.loglikelihood(proposed_state, observation)
         log_weights = log_weights - self._proposal_model.loglikelihood(proposed_state, state, inputs, observation)
         log_weights = log_weights + state.log_weights
-
         log_likelihood_increment = tf.math.reduce_logsumexp(log_weights, 1)
         log_likelihoods = state.log_likelihoods + log_likelihood_increment
         normalized_log_weights = normalize(log_weights, 1, True)
+        normalized_log_weights = tf.clip_by_value(normalized_log_weights, MIN_LOG_WEIGHT * float_n_particles,
+                                                  tf.constant(float('inf')))
         return attr.evolve(proposed_state, weights=tf.math.exp(normalized_log_weights),
                            log_weights=normalized_log_weights, log_likelihoods=log_likelihoods)
 
@@ -90,17 +93,17 @@ class SMC(Module):
 
         data_iterator = iter(observation_series)
 
-        def body(state, states_series, i):
+        def body(state, states, i):
             observation = data_iterator.get_next()
             state = self.update(state, observation, tf.constant(0.))
-            states_series = states_series.write(i, state)
-            return state, states_series, i + 1
+            states = states.write(i, state)
+            return state, states, i + 1
 
-        def cond(_state, _states_series, i):
+        def cond(_state, _states, i):
             return i < n_observations
 
         i0 = tf.constant(0)
-        final_state, states_series, _ = tf.while_loop(cond, body, [initial_state, states_series, i0], )
+        final_state, states, _ = tf.while_loop(cond, body, [initial_state, states_series, i0], )
         return final_state, states_series.stack()
 
     @tf.function
@@ -110,9 +113,10 @@ class SMC(Module):
 
     @tf.function
     def _return_final_loop(self, initial_state: State, observation_series: tf.data.Dataset, n_observations: tf.Tensor):
-        final_state, _ = self._return(initial_state, observation_series, n_observations)
+        final_state, states_series = self._return(initial_state, observation_series, n_observations)
         return final_state
 
+    @tf.function
     def __call__(self, initial_state: State, observation_series: tf.data.Dataset, n_observations: tf.Tensor,
                  return_final=False):
         """

tests/resampling/differentiable/optimal_transport/test_sinkhorn.py
@@ -8,8 +8,8 @@ from filterflow.resampling.differentiable.regularized_transport.plan import tran
 class TestSinkhorn(tf.test.TestCase):
     def setUp(self):
         np.random.seed(42)
-        n_particles = 5
-        batch_size = 1
+        n_particles = 100
+        batch_size = 3
         dimension = 2
 
         self.n_particles = tf.constant(n_particles)
@@ -19,7 +19,7 @@ class TestSinkhorn(tf.test.TestCase):
         self.np_epsilon = 0.25
         self.epsilon = tf.constant(self.np_epsilon)
 
-        self.threshold = tf.constant(1e-2)
+        self.threshold = tf.constant(1e-6)
         self.n_iter = tf.constant(1000)
 
         self.np_x = np.random.uniform(-1., 1., [batch_size, n_particles, dimension]).astype(np.float32)
@@ -36,10 +36,7 @@ class TestSinkhorn(tf.test.TestCase):
     def test_transport(self):
         T_scaled, total_iter_scaled = transport(self.x, self.degenerate_logw, self.epsilon, 0.95, self.threshold,
                                                 self.n_iter, self.n_particles)
-        T_simple, total_iter_simple = transport(self.x, self.degenerate_logw, self.epsilon, 1., self.threshold,
-                                                self.n_iter, self.n_particles)
 
-        self.assertGreater(total_iter_simple, total_iter_scaled)
         self.assertAllClose(tf.constant(self.degenerate_weights) * tf.cast(self.n_particles, float),
                             tf.reduce_sum(T_scaled, 1))
 

tests/resampling/differentiable/ricatti/test_solver.py
@@ -23,8 +23,11 @@ class TestFunctions(tf.test.TestCase):
 
     def test_make_admissible(self):
         admissible = _make_admissible(self.tensor)
-        self._assert_nil(admissible)
         self.assertAllClose(admissible, tf.transpose(admissible, perm=[0, 2, 1]), atol=1e-5)
+        row_sum = tf.reduce_sum(admissible, 1)
+        col_sum = tf.reduce_sum(admissible, 2)
+        self.assertAllClose(row_sum, tf.zeros_like(row_sum), atol=1e-6)
+        self.assertAllClose(col_sum, tf.zeros_like(col_sum), atol=1e-6)
 
 
 class TestRicatti(tf.test.TestCase):
@@ -32,7 +35,7 @@ class TestRicatti(tf.test.TestCase):
         np.random.seed(42)
         self.horizon = tf.constant(5.)
         self.batch_size = 1
-        self.n_particles = 5
+        self.n_particles = 25
         self.dimension = 2
 
         choice = np.random.binomial(1, 0.25, [self.batch_size, self.n_particles]).astype(bool)
@@ -94,9 +97,9 @@ class TestRicatti(tf.test.TestCase):
 
     def test_gradient(self):
         @tf.function
-        def fun(w):
+        def fun_w(w):
             w_ = w / tf.reduce_sum(w, 1)
             return tf.math.reduce_std(self.ricatti_instance(self.transport_matrix, w_))
 
-        theoretical, numerical = tf.test.compute_gradient(fun, [tf.constant(self.w)])
-        self.assertAllClose(theoretical[0], numerical[0], 1e-2)
+        theoretical, numerical = tf.test.compute_gradient(fun_w, [tf.constant(self.w)])
+        self.assertAllClose(theoretical[0], numerical[0], 1e-5)