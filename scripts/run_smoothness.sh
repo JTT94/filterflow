49ce33aaa6ab8dba9740426ed9b18baecc4d30d3
anon284 <jtthornton1994@gmail.com>
2020-05-26 12:21:12 +0100

    update num particles

--- scripts/run_smoothness.sh
+++ scripts/run_smoothness.sh
@@ -1,3 +1,3 @@
-python ./simple_linear_smoothness.py --epsilon=0.25 --n_particles=100 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
-python ./simple_linear_smoothness.py --epsilon=0.5 --n_particles=100 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
-python ./simple_linear_smoothness.py --epsilon=0.75 --n_particles=100 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
+python ./simple_linear_smoothness.py --epsilon=0.25 --n_particles=25 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
+python ./simple_linear_smoothness.py --epsilon=0.5 --n_particles=25 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
+python ./simple_linear_smoothness.py --epsilon=0.75 --n_particles=25 -savefig --T=150 --mesh_size=20 --scaling=0.85 --convergence_threshold=1e-4 --resampling_neff=0.5
