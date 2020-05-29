7cc22fdc41f0262453fea13e0e106211c4b5deaf
AdrienCorenflos <adrien.corenflos@gmail.com>
2020-05-29 13:11:53 +0100

    finalzing examples

--- scripts/simple_linear_variational.py
+++ scripts/simple_linear_variational.py
@@ -1,6 +1,6 @@
 import enum
 import os
-
+import copy
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -18,96 +18,58 @@ from filterflow.resampling.differentiable import PartiallyCorrectedRegularizedTr
 from filterflow.resampling.differentiable.loss import SinkhornLoss
 from filterflow.resampling.differentiable.optimized import OptimizedPointCloud
 from filterflow.resampling.differentiable.optimizer.sgd import SGD
+from scripts.simple_linear_common import get_data, kf_loglikelihood, ResamplingMethodsEnum
 
 
-def get_data(transition_matrix, observation_matrix, transition_covariance, observation_covariance, T=100,
-             random_state=None):
-    if random_state is None:
-        random_state = np.random.RandomState()
-    kf = pykalman.KalmanFilter(transition_matrix, observation_matrix, transition_covariance, observation_covariance)
-    sample = kf.sample(T, random_state=random_state)
-    data = sample[1].data.astype(np.float32)
-    return data.reshape(T, 1, 1, -1)
-
-class ResamplingMethodsEnum(enum.IntEnum):
-    MULTINOMIAL = 0
-    SYSTEMATIC = 1
-    STRATIFIED = 2
-    REGULARIZED = 3
-    VARIANCE_CORRECTED = 4
-    OPTIMIZED = 5
+@tf.function(experimental_relax_shapes=True)
+def get_elbos(pf, initial_state, observations_dataset, T, modifiable_transition_values, values, filter_seed):
+    elbos = tf.TensorArray(dtype=tf.float32, size=values.shape[0])
+    for i in tf.range(values.shape[0]):
+        val = values[i]
+        assign_op = modifiable_transition_values.assign(tf.linalg.diag(val))
+        with tf.control_dependencies([assign_op]):
+            final_state = pf(initial_state, observations_dataset, n_observations=T, return_final=True, seed=filter_seed)
+        elbos = elbos.write(tf.cast(i, tf.int32), final_state.log_likelihoods / tf.cast(T, float))
+    return elbos.stack()
 
 
-def get_gradient_descent_function():
-    # This is a trick because tensorflow doesn't allow you to create variables inside a decorated function
-    @tf.function
-    def gradient_descent(pf, initial_state, observations_dataset, T, gradient_variables, n_iter, optimizer):
-        loss = tf.TensorArray(dtype=tf.float32, size=n_iter + 1, dynamic_size=False)
-        for i in tf.range(n_iter):
-            with tf.GradientTape() as tape:
-                tape.watch(gradient_variables)
-                final_state = pf(initial_state, observations_dataset, T, return_final=True)
-                loss_value = -tf.reduce_mean(final_state.log_likelihoods)
-            loss = loss.write(tf.cast(i, tf.int32), loss_value)
-            grads = tape.gradient(loss_value, gradient_variables)
-            optimizer.apply_gradients(zip(grads, gradient_variables))
-        final_state = pf(initial_state, observations_dataset, T, return_final=True)
-        loss = loss.write(tf.cast(n_iter, tf.int32), -tf.reduce_mean(final_state.log_likelihoods))
-        return [tf.convert_to_tensor(var) for var in gradient_variables], loss.stack()
-
-    return gradient_descent
-
-
-def compare_learning_rates(pf, initial_state, observations_dataset, T, gradient_variables, initial_values, n_iter,
-                           optimizer_maker, learning_rates):
-    loss_profiles = []
-    reset_ops = [k.assign(v) for k, v in zip(gradient_variables, initial_values)]
-    for learning_rate in tqdm(learning_rates):
-        optimizer = optimizer_maker(learning_rate=learning_rate)
-        gradient_descent_function = get_gradient_descent_function()
-        with tf.control_dependencies([reset_ops]):
-            final_variables, loss_profile = gradient_descent_function(pf, initial_state, observations_dataset, T,
-                                                                      gradient_variables, n_iter, optimizer)
-        loss_profiles.append(loss_profile.numpy())
-    return loss_profiles
-
-
-def plot_losses(loss_profiles_df, filename, savefig):
-    fig, ax = plt.subplots(figsize=(5, 5))
-    loss_profiles_df.plot(ax=ax)
-    fig.tight_layout()
+def kalman_main(kf, data, values, T, savefig):
+    log_likelihoods = []
+    for val in values:
+        transition_matrix = np.diag(val)
+        kf_copy = copy.copy(kf)
+        kf_copy.transition_matrices = transition_matrix
+        log_likelihoods.append(kf_loglikelihood(kf_copy, data) / T)
+    likelihoods_df = pd.Series(log_likelihoods,
+                               name=r'$\ell(\theta_1, \theta_2)$',
+                               index=pd.Index(values[:, 0],
+                                              name=r'$\theta_1, \theta_2$')).to_frame().reset_index()
     if savefig:
-        fig.savefig(os.path.join('./charts/', f'variational_different_lr_loss_{filename}.png'))
+        filename = f'kalman_likelihoods_values.tex'
+        likelihoods_df.to_latex(buf=os.path.join('./tables/', filename),
+                                float_format='{:,.3f}'.format, escape=False, index=False)
     else:
-        fig.suptitle(f'variational_different_loss_{filename}')
-        plt.show()
+        print(likelihoods_df.to_latex(float_format='{:,.3f}'.format, escape=False, index=False))
 
 
-def plot_variables(variables_df, filename, savefig):
-    fig, ax = plt.subplots(figsize=(5, 5))
-    variables_df.plot(ax=ax)
-    fig.tight_layout()
-    if savefig:
-        fig.savefig(os.path.join('./charts/', f'variational_different_lr_variables_{filename}.png'))
-    else:
-        fig.suptitle(f'variational_different_lr_variables_{filename}')
-        plt.show()
-
-
-def main(resampling_method_value, resampling_neff, learning_rates=(1e-4, 1e-3), resampling_kwargs=None,
-         T=100, batch_size=1, n_particles=25,
-         data_seed=0, n_iter=50, savefig=False):
+def main(resampling_method_value, resampling_neff, resampling_kwargs=None, T=150, batch_size=50, n_particles=25,
+         data_seed=0, values=(0.25, 0.5, 0.75), filter_seed=555, savefig=False):
     transition_matrix = 0.5 * np.eye(2, dtype=np.float32)
     transition_covariance = np.eye(2, dtype=np.float32)
     observation_matrix = np.eye(2, dtype=np.float32)
     observation_covariance = 0.1 * np.eye(2, dtype=np.float32)
 
+    values = np.array(list(zip(values, values)), dtype=np.float32)
+
     resampling_method_enum = ResamplingMethodsEnum(resampling_method_value)
 
     np_random_state = np.random.RandomState(seed=data_seed)
-    data = get_data(transition_matrix, observation_matrix, transition_covariance, observation_covariance, T,
-                    np_random_state)
+    data, kf = get_data(transition_matrix, observation_matrix, transition_covariance, observation_covariance, T,
+                        np_random_state)
     observation_dataset = tf.data.Dataset.from_tensor_slices(data)
+
+    if resampling_method_enum == 6:
+        return kalman_main(kf, data, values, T, savefig)
 
     if resampling_kwargs is None:
         resampling_kwargs = {}
@@ -148,32 +110,39 @@ def main(resampling_method_value, resampling_neff, learning_rates=(1e-4, 1e-3), 
     observation_covariance_chol = tf.linalg.cholesky(observation_covariance)
 
     initial_particles = np_random_state.normal(0., 1., [batch_size, n_particles, 2]).astype(np.float32)
-    initial_state = State(initial_particles)
+    initial_state = State(tf.constant(initial_particles))
 
     smc = make_filter(observation_matrix, modifiable_transition_matrix, observation_covariance_chol,
                       transition_covariance_chol, resampling_method, resampling_criterion)
 
-    variables = [modifiable_transition_matrix]
+    elbos = get_elbos(smc,
+                      initial_state,
+                      observation_dataset,
+                      tf.constant(T),
+                      modifiable_transition_matrix,
+                      tf.constant(values),
+                      tf.constant(filter_seed))
 
-    def optimizer_maker(learning_rate):
-        # tf.function doesn't like creating variables. This is a way to create them outside the graph
-        # We can't reuse the same optimizer because it would be giving a warmed-up momentum to the ones run later
-        optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
-        return optimizer
+    elbos_df = pd.DataFrame(elbos.numpy(), pd.Index(values[:, 0], name=r'$\theta_1$, $\theta_2$'))
 
-    initial_values = [init_transition_matrix]
+    elbos_df = elbos_df.T.describe().T[['mean', 'std']].reset_index()
 
-    losses = compare_learning_rates(smc, initial_state, observation_dataset, T, variables,
-                                    initial_values, n_iter, optimizer_maker, learning_rates)
-    losses_df = pd.DataFrame(np.stack(losses).T, columns=learning_rates)
-    losses_df.columns.name = 'learning rate'
-    losses_df.columns.epoch = 'epoch'
-
-    plot_losses(losses_df, resampling_method_enum.name, savefig)
+    if savefig:
+        filename = f'{resampling_method_enum.name}_batchsize_{batch_size}_N_{n_particles}_likelihoods_values.tex'
+        elbos_df.to_latex(buf=os.path.join('./tables/', filename),
+                          float_format='{:,.3f}'.format, escape=False, index=False)
+    else:
+        print(elbos_df.to_latex(float_format='{:,.3f}'.format, escape=False, index=False))
 
 
 if __name__ == '__main__':
-    learning_rates = np.logspace(-5, -1, 5, base=10).astype(np.float32)
-    main(ResamplingMethodsEnum.REGULARIZED, 0.5, T=100, n_particles=10, batch_size=100, learning_rates=learning_rates,
-         n_iter=500,
-         resampling_kwargs=dict(epsilon=0.5, scaling=0.75, convergence_threshold=1e-1))
+    for resampling_method in {ResamplingMethodsEnum.VARIANCE_CORRECTED, ResamplingMethodsEnum.REGULARIZED,
+                              ResamplingMethodsEnum.MULTINOMIAL}:
+        for n_particles in {25, 50, 100}:
+            main(resampling_method, 0.5, T=150, n_particles=n_particles, batch_size=100,
+                 resampling_kwargs=dict(epsilon=0.5, scaling=0.75, convergence_threshold=1e-1),
+                 filter_seed=555, data_seed=111, savefig=True)
+
+    main(ResamplingMethodsEnum.KALMAN, 0.5, T=150, n_particles=25, batch_size=100,
+         resampling_kwargs=dict(epsilon=0.5, scaling=0.75, convergence_threshold=1e-1),
+         filter_seed=555, data_seed=111, savefig=True)
