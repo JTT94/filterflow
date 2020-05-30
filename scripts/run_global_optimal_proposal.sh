965f4625d460e3df27a235945f14208af675f2dd
AdrienCorenflos <adrien.corenflos@gmail.com>
2020-05-30 16:12:41 +0100

    script

--- scripts/run_global_optimal_proposal.sh
+++ scripts/run_global_optimal_proposal.sh
@@ -1,8 +1,18 @@
-python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=True --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
-python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=False --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
-python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=True --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
-python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=False --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
-python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=False --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
-python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=True --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=True --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=False --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=True --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=False --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=True --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=False --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=False --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=True --batch_size=1 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=True --batch_size=10 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=0 --change_seed=False --batch_size=10 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=False --batch_size=10 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+python ./global_optimal_proposal_variational.py --resampling_method=3 --change_seed=True --batch_size=10 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.8 --convergence_threshold=1e-3 --resampling_neff=0.5 &
+
+
 python ./global_optimal_proposal_variational.py --resampling_method=4 --change_seed=True --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
 python ./global_optimal_proposal_variational.py --resampling_method=4 --change_seed=False --batch_size=4 --epsilon=0.5 --n_particles=25 -savefig --T=150 --scaling=0.75 --convergence_threshold=1e-3 --resampling_neff=0.5 &
