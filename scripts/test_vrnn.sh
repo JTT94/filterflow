996669eb4591a46546a861442c08da930a38caeb
James Thornton <jtthornton1994@gmail.com>
2022-10-27 11:14:22 +0200

    Delete test_vrnn.sh

--- scripts/test_vrnn.sh
+++
@@ -1,6 +0,0 @@
-python train_vrnn.py --epsilon=0.8 --resampling_neff=0.5 --scaling=0.9 \
-                     --convergence_threshold=1e-3  \
-                     --initial_lr=0.01 --decay 0.9 --decay_steps=200 \
-                     --n_iter=1000 --max_iter=1000  --n_particles=100 \
-                     --out_dir="./test"  \
-                     --filter_seed=42 --data_seed=0 -nofixed_filter_seed
